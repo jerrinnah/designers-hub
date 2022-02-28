4a33177b717e7215db9e8f0686e5b6f92d90b3fa
NEJ <76699731+jerrinnah@users.noreply.github.com>
2022-02-28 11:51:49 +0100

    initial commit

---
+++ src/js/utils.js
@@ -0,0 +1,13 @@
+export function add(a, b) {
+  return a + b;
+}
+
+export const sub = function (a, b) {
+  return a - b;
+};
+
+export const multi = (a, b) => a * b;
+
+export const div = (a, b) => {
+  return a / b;
+};
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "./styles/scss/main.scss";
+import { add, sub, multi, div } from "./js/utils";
+
+console.log(add(1, 2));
+console.log(sub(8, 2));
+console.log(multi(4, 2));
+console.log(div(20, 2));
